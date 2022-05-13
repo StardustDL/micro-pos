a47cecfecab93fe4203d9ed938e00008e8bca1ac
StardustDL <stardustdl@163.com>
2022-05-13 23:12:10 +0800

    add basic build script

---
+++ build/entrypoint.java
@@ -0,0 +1,11 @@
+package com.micropos.{project};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class {Project}Application {
+    public static void main(String[] args) {
+        SpringApplication.run({Project}Application.class, args);
+    }
+}
---
+++ buildcox.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+import os
+from coxbuild.schema import task, group, named, run as prun, depend, ext, withExecutionState, ExecutionState, withProject, ProjectSettings, withConfig, Configuration
+
+
+root = Path(".")
+
+
+def ensureDir(dir: Path):
+    if dir.exists() and dir.is_dir():
+        return
+    os.makedirs(dir)
+
+
+@withConfig
+@task
+def create(config: Configuration):
+    name: str = config.get("name") or ""
+    if not name:
+        print("No name specified")
+        return
+    subroot = root / name
+    ensureDir(subroot)
+    Path(subroot / "pom.xml").write_text(
+        Path("./build/pom.xml").read_text().replace(r"{project}", name))
+
+    Name = name.title()
+    src = subroot / "src"
+    ensureDir(src / "main" / "java" / "com" / "micropos" / name)
+    Path(src / "main" / "java" / "com" / "micropos" / name / f"{Name}Application.java").write_text(
+        Path("./build/entrypoint.java").read_text().replace(r"{project}", name).replace(r"{Project}", Name))
+    ensureDir(src / "resources")
+
+
+@withConfig
+@task
+def run(config: Configuration):
+    name: str = config.get("name") or ""
+    if not name:
+        print("No name specified")
+        return
+    prun(["mvn", "spring-boot:run"], cwd=root / name, shell=True)
+
+
+@withConfig
+@task
+def compile(config: Configuration):
+    name: str = config.get("name") or ""
+    if not name:
+        for subdir in root.iterdir():
+            if subdir.is_dir() and (subdir / "pom.xml").exists():
+                prun(["mvn", "compile"], cwd=subdir, shell=True)
+    else:
+        prun(["mvn", "compile"], cwd=root / name, shell=True)
