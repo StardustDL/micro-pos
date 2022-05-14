4b7fe33fa8538b1b142dec4ecdfaeb7cd7cfef13
StardustDL <stardustdl@163.com>
2022-05-14 17:43:55 +0800

    add build artifacts

--- buildcox.py
+++ buildcox.py
@@ -1,10 +1,17 @@
 from pathlib import Path
 import os
 from platform import system
+import shutil
 from coxbuild.schema import task, group, named, run as innerRun, depend, ext, withExecutionState, ExecutionState, withProject, ProjectSettings, withConfig, Configuration
 
 
 root = Path(".")
+
+
+def projects():
+    for subdir in root.iterdir():
+        if subdir.is_dir() and (subdir / "build.gradle").exists():
+            yield subdir.name
 
 
 def prun(*args, **kwargs):
@@ -29,12 +36,43 @@ def run(config: Configuration):
 
 @withConfig
 @task
-def default(config: Configuration):
+def test(config: Configuration):
+    def onetest(name: str):
+        subdir = root / name
+        print(f"Testing {subdir.name}")
+        prun(["gradle", "test"], cwd=subdir)
+
     name: str = config.get("name") or ""
     if not name:
-        for subdir in root.iterdir():
-            if subdir.is_dir() and (subdir / "build.gradle").exists():
-                print(f"Compiling {subdir.name}")
-                prun(["gradle", "build"], cwd=subdir)
+        for project in projects():
+            onetest(project)
     else:
-        prun(["gradle", "build"], cwd=root / name)
+        onetest(name)
+
+
+@withConfig
+@task
+def default(config: Configuration):
+    def onebuild(name: str):
+        subdir = root / name
+        print(f"Building {subdir.name}")
+        prun(["gradle", "build"], cwd=subdir)
+
+    name: str = config.get("name") or ""
+    if not name:
+        for project in projects():
+            onebuild(project)
+    else:
+        onebuild(name)
+
+
+@depend(default, test)
+@task
+def ci():
+    ciroot = root / "build" / "ci"
+    ensureDir(ciroot)
+    for project in projects():
+        target = ciroot / project
+        if target.exists():
+            shutil.rmtree(target)
+        shutil.copytree(root / project / "build", ciroot / project)
