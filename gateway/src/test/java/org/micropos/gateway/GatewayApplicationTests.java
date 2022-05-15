4ede3f7493ba0a88681e1e30353f919e1f2134d1
StardustDL <stardustdl@163.com>
2022-05-15 12:40:09 +0800

    fix unused test

--- gateway/src/test/java/org/micropos/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/org/micropos/gateway/GatewayApplicationTests.java
@@ -14,21 +14,4 @@ import static org.assertj.core.api.BDDAssertions.then;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GatewayApplicationTests {
-
-	@LocalServerPort
-	private int port;
-
-	@Autowired
-	private TestRestTemplate testRestTemplate;
-
-	private String url(String endpoint) {
-		return "http://localhost:" + this.port + endpoint;
-	}
-
-	@Test
-	public void shouldStartEurekaServer() {
-		ResponseEntity<String> entity = this.testRestTemplate.getForEntity(this.url("/eureka/apps"), String.class);
-
-		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-	}
 }
